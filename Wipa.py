f6e83792fc4e4e9f15a3319d0322d60f9d8f4736
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-14 21:18:49 +0200

    Wipa.py

--- Wipa.py
+++ Wipa.py
@@ -1,1 +1,13 @@
+import string
+import random
+import os
 
+while True:
+    password_length = random.randint(8, 20)
+    password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(password_length))
+    response = os.system(f"ping -n 1 -w 1 192.168.1.1 -w 500 > nul && ping -n 1 -w 1 google.com -w 500 > nul && ping -n 1 -w 1 yahoo.com -w 500 > nul")
+    if response == 0:
+        print(f"Correct password found: {password}")
+        break
+    else:
+        print(f"Incorrect password: {password}")
